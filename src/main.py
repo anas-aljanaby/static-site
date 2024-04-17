c97bba3eaecc407fe56f2f2cd00239e03e9a73c3
anas-aljanaby <anas.aljanaby00@gmail.com>
2024-04-18 02:17:43 +0300

    added copy script

--- src/main.py
+++ src/main.py
@@ -9,12 +9,15 @@ shutil.rmtree(public_path)
 def traverse_dir(path):
     for p in os.listdir(path):
         if os.isdir(p):
+            shutil.copy(p, public_path)
+            print('INFO: Copied dir {p} to public dir')
             return traverse_dir(p)
         else:
+            print('INFO: Copied file {p} to public dir')
             shutil.copy(p, public_path)
 
 
-traverse_dir()
+traverse_dir(pwd)
 
 
 
